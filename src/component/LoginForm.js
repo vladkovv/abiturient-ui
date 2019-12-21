3303a3d58d860f69333351f688635c14d78eed09
Андрей Бизюк <biziuk.andrei@gmail.com>
2019-12-21 21:25:17 +0300

    try login

--- src/component/LoginForm.js
+++ src/component/LoginForm.js
@@ -5,11 +5,62 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+
+function Message(props){
+    let type = "primary";
+    let message = "";
+    if(props.error){
+        type = "danger";
+        message = props.error;
+    }else if(props.message){
+        type = "success";
+        message = props.message;
+    }
+
+    if(message){
+        return (
+            <Alert variant={type}>
+                {message}
+            </Alert>
+        );
+    }else{
+        return "";
+    }
+}
 
 export default class LoginForm extends React.Component {
     constructor(props) {
         super(props);
         this.props = props;
+        this.tryLogin = this.tryLogin.bind(this);
+        this.loginChange = this.loginChange.bind(this);
+        this.passwordChange = this.passwordChange.bind(this);
+        this.state = {login:"", password:"", error:"", message:""};
+    }
+
+    loginChange(event){
+        this.setState({login: event.target.value});
+    }
+
+    passwordChange(event){
+        this.setState({password: event.target.value});
+    }
+
+    async tryLogin(){
+        let response = await fetch("http://localhost:3000/auth/login", {
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify({username:this.state.login, password:this.state.password})
+        });
+
+        let result = await response.json();
+
+        if(result.error){
+            this.setState({error:result.error, message:""});
+        }else{
+            this.setState({message:"Успешно!", error:""});
+        }
     }
 
     render() {
@@ -17,7 +68,7 @@ export default class LoginForm extends React.Component {
             <Form>
                 <Form.Group controlId="formLogin">
                     <Form.Label>Логин</Form.Label>
-                    <Form.Control type="text" placeholder="Логин" />
+                    <Form.Control type="text" placeholder="Логин" onChange={this.loginChange}/>
                     <Form.Text className="text-muted">
                         Введите логин для входа
                     </Form.Text>
@@ -25,11 +76,12 @@ export default class LoginForm extends React.Component {
 
                 <Form.Group controlId="formPassword">
                     <Form.Label>Пароль</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={this.passwordChange}/>
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" onClick={this.tryLogin}>
                     Войти
                 </Button>
+                <Message message={this.state.message} error={this.state.error}/>
             </Form>
         )
     }
