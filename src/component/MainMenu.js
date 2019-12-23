b6c8934948a6970f106f532a9632f6c7edf1e596
Андрей Бизюк <biziuk.andrei@gmail.com>
2019-12-23 14:39:24 +0300

    eventkeys

--- src/component/MainMenu.js
+++ src/component/MainMenu.js
@@ -1,58 +1,52 @@
-import React, { Component } from "react";
+import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
 
 export default class MyButton extends React.Component{
     constructor(props){
         super(props);
         this.props = props;
         this.handleHomeLink = this.handleHomeLink.bind(this);
-        this.handleLoginLink = this.handleLoginLink.bind(this);
-        this.handleAbitursLink = this.handleAbitursLink.bind(this);
+        this.handleMenuSelect = this.handleMenuSelect.bind(this);
     }
 
     handleHomeLink(){
         this.props.handlePageChange('home');
     }
 
-    handleLoginLink(){
-        this.props.handlePageChange('login');
-    }
-
-    handleAbitursLink(){
-        this.props.handlePageChange('abiturs');
+    handleMenuSelect(eventKey){
+        this.props.handlePageChange(eventKey);
     }
 
     render(){
-        return (<Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home"
-                onClick={this.handleHomeLink}>
-                    Регистрация<br/>Абитуриентов
+        return (<Navbar bg="dark" variant="dark">
+            <Navbar.Brand>
+                Регистрация<br />Абитуриентов
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#login" 
-                        onClick={this.handleLoginLink}>
-                            Вход
+                <Nav className="mr-auto" onSelect={this.handleMenuSelect}>
+                    <Nav.Link eventKey="home">
+                        Домой
                     </Nav.Link>
-                    <Nav.Link href="#abiturs"
-                        onClick={this.handleAbitursLink}>
-                            Абитуриенты
+                    <Nav.Link eventKey="login">
+                        Вход
+                    </Nav.Link>
+                    <Nav.Link eventKey="abiturs">
+                        Абитуриенты
                     </Nav.Link>
                     <NavDropdown title="Справочники" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/specs">
+                        <NavDropdown.Item eventKey="specs">
                             Специальности
-              </NavDropdown.Item>
-                        <NavDropdown.Item href="#action/groups">Группы</NavDropdown.Item>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item eventKey="groups">
+                            Группы
+                            </NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/users">
+                        <NavDropdown.Item eventKey="users">
                             Пользователи
-              </NavDropdown.Item>
+                        </NavDropdown.Item>
                     </NavDropdown>
                 </Nav>                
             </Navbar.Collapse>
